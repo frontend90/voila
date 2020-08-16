75f482030ccf760e0d2d0c81127b2a8553025e3d
Vasiliy Pupkin <vpupkin@company.com>
2020-08-16 20:25:12 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+/*********************     плавный скролл        ************************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
+
 /************    burger  (открыть, закрыть)    *************/
 let user_icon = document.querySelector('.burger');
 user_icon.addEventListener("click", function (e) {
